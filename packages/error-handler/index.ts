9e17d9d7844861c1616351351646befc46e394a3
ashmit65 <ashmits554@gmail.com>
2026-04-08 23:35:54 +0530

    pushed

--- packages/error-handler/index.ts
+++ packages/error-handler/index.ts
@@ -1,4 +1,12 @@
 export class AppError extends Error {
     public readOnly statusCode: number;
     public readOnly isOperational: boolean; 
+    public readOnly details? : any;
+
+    constructor(
+        message: string,
+        statusCode: number,
+        isOperational = true,
+        details?: any
+    )
 }
