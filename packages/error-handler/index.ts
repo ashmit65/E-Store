e0e6f320d7ab3f0a1e48489dfdbf9bfc882655ca
ashmit65 <ashmits554@gmail.com>
2026-04-08 23:48:31 +0530

    add custom AppError class for structured error handling

--- packages/error-handler/index.ts
+++ packages/error-handler/index.ts
@@ -1,12 +1,18 @@
 export class AppError extends Error {
-    public readOnly statusCode: number;
-    public readOnly isOperational: boolean; 
-    public readOnly details? : any;
+    public readonly statusCode: number;
+    public readonly isOperational: boolean; 
+    public readonly details? : any;
 
     constructor(
         message: string,
         statusCode: number,
         isOperational = true,
         details?: any
-    )
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = isOperational;
+        this.details = details;
+        Error.captureStackTrace(this);
+    }
 }
