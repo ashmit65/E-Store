170097a225c464200d2d9825334c7690b135e8cb
ashmit65 <ashmits554@gmail.com>
2026-04-15 23:15:28 +0530

    added ejs

--- apps/auth-service/src/utils/sendMail/index.ts
+++ apps/auth-service/src/utils/sendMail/index.ts
@@ -6,10 +6,29 @@ import path from 'path'
 dotenv.config()
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP.HOST,
-    port: Number(process.env.SMTP.PORT) || 587,
-    service: process.env.SMTP.SERVER,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT) || 587,
+    service: process.env.SMTP_SERVER,
     auth: {
-
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
     }
 })
+
+//Render an EJS email template
+
+export const renderTemplate = async (templateName: string, data: Record<string, any>) => {
+    const templatePath = path.join(
+        __dirname,
+        process.cwd(),
+        'auth-service',
+        'src',
+        'utils',
+        'email-templates',
+        `${templateName}.ejs`
+    );
+    return ejs.renderFile(templatePath, data);
+}
+
+
+// send an email using nodemailer 
