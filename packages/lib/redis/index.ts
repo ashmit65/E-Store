122c46c9f39791b41086f78261ffa7e7c5a0b7cb
ashmit65 <ashmits554@gmail.com>
2026-05-01 00:11:46 +0530

    changed redis configuation

--- packages/lib/redis/index.ts
+++ packages/lib/redis/index.ts
@@ -1,9 +1,9 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: Number(process.env.REDIS_PORT) || 6379,
-    password: process.env.REDIS_PASSWORD,
-})
+const redis = new Redis(process.env.REDIS_DATABASE_URL! as string);
+
+redis.on('error', (err) => {
+  // Gracefully handle redis errors
+});
 
 export default redis;
