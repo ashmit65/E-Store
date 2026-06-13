a2313afa19489281aa6a45a4c988d1483a70c154
ashmit65 <ashmits554@gmail.com>
2026-06-13 20:13:26 +0530

    refactor(users-ui): move assets and shared widgets out of routing directory

--- apps/users-ui/src/app/shared/widgets/header/header-bottom.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HeaderBottom = () => {
-    return (
-        <div>HeaderBottom</div>
-    )
-}
-
-export default HeaderBottom;
--- apps/users-ui/src/shared/widgets/header/header.tsx
+++ apps/users-ui/src/shared/widgets/header/header.tsx
@@ -16,17 +16,17 @@ const Header = () => {
                     </Link>
                 </div>
                 <div className='w-[50%] relative h-[45px] flex items-center'>
-                    <input type="text" placeholder='Search for Products...' 
-                    className='w-full px-4 font-Poppins font-medium border-[2.5px] border-[#3489ff] outline-none h-full rounded-lg'
+                    <input type="text" placeholder='Search for Products...'
+                        className='w-full px-4 font-Poppins font-medium border-[2.5px] border-[#3489ff] outline-none h-full rounded-lg'
                     />
                     <div className='w-[60px] cursor-pointer flex items-center justify-center h-full bg-[#3489ff] absolute top-0 right-0 rounded-r-lg'>
-                        <Search color = "#ffff"/>
+                        <Search color="#ffff" />
                     </div>
                 </div>
                 <div className='flex items-center gap-8'>
                     <div className="flex items-center gap-2">
                         <Link href={"/login"}
-                        className='border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]'>
+                            className='border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]'>
                             <ProfileIcon />
                         </Link>
                     </div>
@@ -37,21 +37,21 @@ const Header = () => {
                 </div>
                 <div className='flex items-center gap-5'>
                     <Link href={'/wishlist'} className='relative'>
-                        <HeartIcon/>
+                        <HeartIcon />
                         <div className='w-5 h-5 border-2 border-white bg-red-400 rounded-full flex items-center justify-center absolute top-[-10px] right-[-10px] text-white text-[12px] font-medium'>
                             <span className='text-white font-medium text-xs'>0</span>
                         </div>
                     </Link>
                     <Link href={'/cart'} className='relative'>
-                        <CartIcon/>
+                        <CartIcon />
                         <div className='w-5 h-5 border-2 border-white bg-red-400 rounded-full flex items-center justify-center absolute top-[-10px] right-[-10px] text-white text-[12px] font-medium'>
                             <span className='text-white font-medium text-xs'>0</span>
                         </div>
                     </Link>
                 </div>
             </div>
-            <div className='border-b border-b-[#99999999]'/>
-                <HeaderBottom/>
+            <div className='border-b border-b-[#99999999]' />
+            <HeaderBottom />
         </div>
     )
 }
