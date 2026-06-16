e7165ac8f0657b47ad9a85f8d4a6ea499207ba00
ashmit65 <ashmits554@gmail.com>
2026-06-16 23:29:28 +0530

    feat(users-ui): create base login layout and google button component

--- apps/users-ui/src/app/(routes)/login/page.tsx
+++ apps/users-ui/src/app/(routes)/login/page.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from 'next/navigation';
 import {useForm} from 'react-hook-form';
 import React, { useState } from 'react';
+import Link from 'next/link';
+import GoogleSVG from 'apps/users-ui/src/shared/components/google-button';
 
 type FormData = {
     email: string;
@@ -20,13 +22,52 @@ const Login = () => {
         
     }
     return (
-        <div className='w-full py-10 min-h-[85vh] bg-rgba(169, 27, 169, 0.18)'>
+        <div className='w-full py-10 min-h-[85vh] bg-[#f1f1f1]'>
             <h1 className='text-4xl font-Poppins font-semibold text-black text-center'>
                 Login
             </h1>
             <p className='text-center text-lg font-medium py-3 text-[#0000005c]'>
                 Home . Login
             </p>
+
+            <div className='w-full flex justify-center'>
+                <div className='md:w-[480px] p-8 bg-white shadow rounded-lg'>
+                    <h3 className='text-3xl font-semibold text-center mb-2'>
+                        Login to Eshop
+                    </h3>
+                    <p className='text-center text-gray-500 mb-4'>
+                        Don't have Account? {" "}
+                        <Link href={'/signup'} className='text-blue-500'>
+                            Sign Up
+                        </Link>
+                    </p>
+                    <GoogleSVG />
+                    <div className='flex items-center my-5 text-gray-400 text-sm'>
+                        <div className='flex-1 border-t border-gray-300' />
+                        <span className='px-3'>or Sign in with Email</span>
+                        <div className='flex-1 border-t border-gray-300' />
+                    </div>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <label className='block text-gray-700 mb-1'>Email</label>
+                        <input type="email"
+                        placeholder='ashmits554@gmail.com'
+                        className='w-full p-2 border border-gray-300 outline-0 rounded mb-1'
+                        {...register('email', {
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: "Invalid email format"
+                            }
+                        })}
+                        />
+                        {errors.email && (
+                            <p className='text-red-500 text-sm'>{String(errors.email.message)}</p>
+                        )}
+                        
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
